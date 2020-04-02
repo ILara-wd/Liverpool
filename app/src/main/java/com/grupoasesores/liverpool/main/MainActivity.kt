55bc2be07499a4262d984474c63fa39b257fdaa6
Irving Lara <irving@gaiadesign.com.mx>
2020-04-02 16:54:48 -0600

    action button y editText

--- app/src/main/java/com/grupoasesores/liverpool/main/MainActivity.kt
+++ app/src/main/java/com/grupoasesores/liverpool/main/MainActivity.kt
@@ -2,6 +2,8 @@ package com.grupoasesores.liverpool.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.grupoasesores.liverpool.R
 import com.grupoasesores.liverpool.service.model.APIError
@@ -11,16 +13,28 @@ import com.grupoasesores.liverpool.service.model.Record
 class MainActivity : AppCompatActivity(), ImplMainView {
 
     private lateinit var presenter: MainPresenter
+    private lateinit var tvSearch: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         presenter = MainPresenter(this)
+        initView()
     }
 
-    override fun onResume() {
-        presenter.executeService(QueryOutput("silla", 1))
-        super.onResume()
+    private fun initView() {
+        tvSearch = findViewById(R.id.tv_search)
+
+        findViewById<Button>(R.id.btn_search)
+            .setOnClickListener{
+                val searchText = tvSearch.text.toString()
+                if (searchText.isNotEmpty()){
+                    tvSearch.setText("")
+                    presenter.executeService(QueryOutput(searchText, 1))
+                }else{
+                    tvSearch.error = getString(R.string.text_et_search)
+                }
+        }
     }
 
     override fun showProducts(record: List<Record>) {
